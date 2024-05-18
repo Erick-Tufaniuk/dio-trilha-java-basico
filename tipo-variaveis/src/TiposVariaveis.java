857a2ee2fff0ef6eccea057adf9db7692b565821
Erick-Tufaniuk <octoniuk@gmail.com>
2024-05-18 20:05:39 -0300

    tipos primitivos e constante

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        
+        String meuNome = "Erick Tufaniuk";
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        // short numeroCurto2 = numeroNormal;
+        //erro de conversao
+
+        // Neste caso usamos o CAST (converte manualmente o tipo da varivel)]
+        short numeroCurto2 = (short) numeroNormal; 
+
+
+        /* CONSTANTE EM JAVA - SE DECLARA SEMPRE COM O NOME DA VARIAVEL EM 
+        CAIXA ALTA E ANTES DO TIPO DA VARIAVEL USA (final) PARA QUE DE FATO SEJA
+        UMA CONSTANTE*/
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
