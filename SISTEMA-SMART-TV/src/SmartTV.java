49c64c2d86cd2b93c707946069d974a19d65f71b
Erick-Tufaniuk <octoniuk@gmail.com>
2024-05-19 16:42:59 -0300

    Instruções sobre javadoc e comentarios

--- SISTEMA-SMART-TV/src/SmartTV.java
+++ SISTEMA-SMART-TV/src/SmartTV.java
@@ -1,10 +1,25 @@
+/** 
+ * <h1>Smart Tv</h1>
+ * Funcionalidades de funcionamento de uma Smart TV.
+ * <br>
+ * <b>Nota:</b> Leia atentamente a documentação desta Classe para usufruir dos recursos
+ * oferecidos pelo autor.
+ * 
+ * @author Erick Tufaniuk
+ * @version 1.0
+ * @since 19/05/2024 */
+ 
+ //ACIMA UMA BREVE DOCUMENTAÇÃO EXPLICANDO E DANDO MAIS DETALHES SOBRE O CODIGO A SEGUIR
+
+
+
 public class SmartTV{
 
     boolean ligada = false;
     int canal = 1;
     int volume = 25;
 
-
+  
     public void ligar() {
         ligada = true;
     }
@@ -32,6 +47,12 @@ public class SmartTV{
         canal--;
         System.out.println("Canal: "+ canal);
     }
+
+/**
+ * Este metodo é usado para acessar o canal recebido manualmente do usuario
+ * @param canalDigitado - este é o primeiro e unico paramentro recebido
+ * @return - N/A
+ */
 
     public void canalManual(int canalDigitado){
         canal = canalDigitado;
